7cc1020fe9a53689536a9922f4aaad9a0d49de73
Ryan Talatagod <michaelbenedictlryan@gmail.com>
2021-10-08 19:31:50 +0800

    Updated

---
+++ src/robots.js
@@ -0,0 +1,34 @@
+export const robots = [
+ {
+ 	id: 1,
+ 	name: 'Ryan Talatagod',
+ 	username: 'Emperor',
+ 	email: 'ryan@gmail.com'
+ },
+ {
+ 	id: 2,
+ 	name: 'Yanni Jan Mondejar',
+ 	username: 'Mons',
+ 	email: 'yanni@gmail.com'
+ },
+ {
+ 	id: 3,
+ 	name: 'Jarry Emorecha',
+ 	username: 'Jack',
+ 	email: 'jarry@gmail.com'
+ },
+ {
+ 	id: 4,
+ 	name: 'Lucelle Sumalinog',
+ 	username: 'Carmebree',
+ 	email: 'lucelle@gmail.com'
+ },
+ {
+ 	id: 5,
+ 	name: 'Diane Isabelle Lambojo',
+ 	username: 'Taehyung',
+ 	email: 'diane@gmail.com'
+ },
+
+]
+
